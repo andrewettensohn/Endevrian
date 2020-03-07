36948205517bdbc2de816c7aa2f70caefb753afe
Andrew Ettensohn <andrewettensohn@gmail.com>
2020-03-07 14:16:02 -0500

    Changed folder structure for models. Added models for sessions.

---
+++ Models/SessionModels/SessionSection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Endevrian.Models
+{
+    public class SessionSection
+    {
+        public int SessionSectionID { get; set; }
+
+        public string UserId { get; set; }
+
+        public int CampaignID { get; set; }
+
+        public string SessionSectionName { get; set; }
+
+    }
+}
