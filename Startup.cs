4b0b70cd5eded9a3548e5ca89378e1ae28bef15d
Andrew Ettensohn <andrewettensohn@gmail.com>
2020-06-28 11:03:03 -0400

    sorted out some non PRD code

--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,6 @@ namespace Endevrian
             using (ApplicationDbContext db = new ApplicationDbContext(new DbContextOptions<ApplicationDbContext>()))
             {
                 db.Database.EnsureCreated();
-                //db.Database.Migrate();
             }
         }
 
@@ -62,25 +61,24 @@ namespace Endevrian
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            string userContentPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "UserContent");
+            //string userContentPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "UserContent");
 
-            if (!Directory.Exists(userContentPath))
-            {
-                Directory.CreateDirectory(userContentPath);
-            }
+            //if (!Directory.Exists(userContentPath))
+            //{
+            //    Directory.CreateDirectory(userContentPath);
+            //}
 
-            app.UseFileServer(new FileServerOptions
-            {
-                FileProvider = new PhysicalFileProvider(userContentPath),
-                RequestPath = "/UserContent",
-                EnableDirectoryBrowsing = true
-            });
+            //app.UseFileServer(new FileServerOptions
+            //{
+            //    FileProvider = new PhysicalFileProvider(userContentPath),
+            //    RequestPath = "/UserContent",
+            //    EnableDirectoryBrowsing = true
+            //});
 
             //This will control the max file upload size. It probably needs to be 100 MB
             app.Use(async (context, next) =>
